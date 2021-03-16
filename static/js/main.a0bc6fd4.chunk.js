81ea391508a0805f9726389e9466a5d652df3544
cyborgx0x <leeboykt@gmail.com>
2021-03-17 03:09:51 +0700

    gen by react

---
+++ static/js/main.a0bc6fd4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-learning"]=this["webpackJsonpreact-learning"]||[]).push([[0],{9:function(e,n,r){"use strict";r.r(n);r(1);var t=r(3),c=r.n(t),i=r(0),s=function(){return Object(i.jsx)("h1",{children:"New Header"})},a=function(){return Object(i.jsx)("div",{children:Object(i.jsx)(s,{})})};c.a.render(Object(i.jsx)(a,{}),document.querySelector("#root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.a0bc6fd4.chunk.js.map
